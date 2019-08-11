95fe766d6560d2f99b416e60bf26f4060c9fa9ae
Administrator <Administrator@Admin>
2019-08-11 22:58:27 +0700

    up code

---
+++ TestKing/src/TestKingMain.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class TestKingMain {
+
+	public void a() {
+		try {
+			throw new IOException ();
+		} catch (IOException e) {
+			System.out.println("A");
+		}catch (Exception e) {
+			System.out.println("B");
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		TestKingMain testKingMain=new TestKingMain();
+		testKingMain.a();
+	}
+
+}
